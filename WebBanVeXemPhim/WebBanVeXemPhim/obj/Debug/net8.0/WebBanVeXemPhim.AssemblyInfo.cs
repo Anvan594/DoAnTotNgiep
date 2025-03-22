fbdffbab599e66fa77e034f86520a9e0b8d34c90
Nguyễn Văn An <vanan9524@gmail.com>
2025-03-22 13:44:35 +0700

    update

--- WebBanVeXemPhim/WebBanVeXemPhim/obj/Debug/net8.0/WebBanVeXemPhim.AssemblyInfo.cs
+++ WebBanVeXemPhim/WebBanVeXemPhim/obj/Debug/net8.0/WebBanVeXemPhim.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBanVeXemPhim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+982531933fce5bcf2926a2e822e12fd245705a0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7776c714c807ab544256bc8549ea77dd4cae11f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBanVeXemPhim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBanVeXemPhim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
